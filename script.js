505a2c2c09b6f19d1311a87b618cfa354e94f533
Bilawal <bilawalkhan57777@gmail.com>
2025-12-04 20:02:49 +0500

    some changes

---
+++ script.js
@@ -0,0 +1,21 @@
+function calculate_criteria(event) {
+
+    event.preventDefault();
+    let username = document.getElementById("username").value
+    let age = document.getElementById("age").value
+    let experience = document.getElementById("experience").value
+    let percentage = document.getElementById("percentage").value
+
+    let result = ""
+
+    if (age <= 20 || experience < 1 || percentage < 70) {
+        result = "Sorry!! " + username + "You Are Not Eligible For Our Criteria"
+    } else {
+        result = "Congrats!! " + username + " You Are Hiring For Teaching"
+    }
+
+    document.getElementById("output").innerText = result
+
+}
+
+
